92db1087cae39fcaa462a5844640e8d11db1267a
Ayban <mrkvnhrnndz17@gmail.com>
2023-05-03 19:33:11 +0800

    Initial Commit- hero to about

---
+++ src/app.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", () => {
+  smoothScroll();
+});
+
+const goTo = (url) => {
+  window.open(url);
+};
+
+const smoothScroll = () => {
+  const links = document.querySelectorAll("a[href^='#']");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute("href"));
+      target.scrollIntoView({ behavior: "smooth" });
+      console.log(target);
+    });
+  });
+};
