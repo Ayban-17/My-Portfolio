6ff2cffe5f4cd3e4ecdfb1c6ae2f494ec0e3aebf
Ayban <mrkvnhrnndz17@gmail.com>
2023-05-04 17:58:24 +0800

    project section

--- src/project.js
+++ src/project.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "YouTube Clone",
       description:
         "This is a YouTube clone website with responsive design, search, and featured videos using HTML, CSS, and JavaScript. Demonstrated web development proficiency and experience implementing complex features.",
-      image: "/assets/projects/youtube.png",
+      image: "assets/projects/youtube.png",
       github_url: "https://github.com/Ayban-17/YouTube-Clone",
       live_demo: "https://ayban-17.github.io/YouTube-Clone/",
     },
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "Space Tourism ",
       description:
         "This is a space tourism website with a modern design using HTML, CSS, and JavaScript. Website features an animated hero section, informative sections with images, and a responsive layout.",
-      image: "/assets/projects/space.png",
+      image: "assets/projects/space.png",
       github_url: "https://github.com/Ayban-17/space-tourism",
       live_demo: "https://ayban-17.github.io/space-tourism/ ",
     },
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "BulSU  Archive",
       description:
         "This is a functional frontend for BulSU Archive website using HTML, CSS, and JavaScript. Website features an intuitive and user-friendly interface, responsive design, and dynamic search functionality.",
-      image: "/assets/projects/archive.png",
+      image: "assets/projects/archive.png",
       github_url:
         "https://github.com/Ayban-17/BulSU-Archive-funtional-frontend-Javscript-html-css",
       live_demo:
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "Moviefy",
       description:
         "This is a simple Movie App using an API with HTML, CSS, and JavaScript. The app features a search function for movies. ",
-      image: "/assets/projects/Moviefy.png",
+      image: "assets/projects/Moviefy.png",
       github_url: "https://github.com/Ayban-17/Movie-App",
       live_demo: "https://ayban-17.github.io/Movie-App/",
     },
